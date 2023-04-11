3692f2a9d6f20358badee7946f8b9c3d70c8a5bf
NataaNK <100471976@alumnos.uc3m.es>
2023-04-11 16:29:08 +0200

    Co-authored-by: 100475053 <estoyaquipormario@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 """
-Autores
+Autores: Natalia Rodríguez Navarro y Ángela Serrano Casas
+
+
 """
+
+import csv
+
+# Cargamos las tablas de probabilidades como una matriz
+with open("./PROB_ON.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  primero = True
+  matriz = []
+  for row in csvreader:
+    if primero:
+        primero = False
+    elif not primero:
+        linea = []
+        for data in row:
+            linea.append(float(data))
+        matriz.append(linea)
+
+print(matriz)
+
+
